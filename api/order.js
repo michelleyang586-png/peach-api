247efe73f83b80669adabf5214fdcce3c9dbb3a5
michelleyang586-png <michelleyang586@gmail.com>
2026-05-24 11:28:45 +0800

    Update order.js

--- api/order.js
+++ api/order.js
@@ -249,6 +249,8 @@ export default async function handler(req, res) {
 
     return res.json({ totalStock, soldStock, remainStock, specs });
 
+  
   } catch (err) {
     return res.status(500).json({ status: 'error', message: err.message });
   }
+}
